656f3320a195eee77f5f9912088e1232e8bfbf64
Md. Abu Musa <33667364+musafci@users.noreply.github.com>
2023-11-07 13:03:04 +0600

    use middleware & install redux-logger package

---
+++ middleware_redux_logger.js
@@ -0,0 +1,53 @@
+const { createStore, applyMiddleware } = require("redux");
+const { default: logger } = require("redux-logger");
+
+// Constant
+const GET_PRODUCTS = "GET_PRODUCTS";
+const ADD_PRODUCT = "ADD_PRODUCT";
+
+
+// Initial state
+const initialProductState = {
+    products: ["sugar", "salt"],
+    numberofProducts: 2,
+}
+
+// Action
+const getProducts = () => {
+    return {
+        type: GET_PRODUCTS
+    }
+}
+const addProduct = (product) => {
+    return {
+        type: ADD_PRODUCT,
+        payload: product,
+    }
+}
+
+// Reducer
+const productReducer = (state = initialProductState, action) => {
+    switch (action.type) {
+        case GET_PRODUCTS:
+            return {
+                ...state,
+            };
+        case ADD_PRODUCT:
+            return {
+                products: [...state.products, action.payload],
+                numberofProducts: state.numberofProducts + 1,
+            }
+    
+        default:
+            return state;
+    }
+}
+
+// store
+const store = createStore(productReducer, applyMiddleware(logger));
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(getProducts());
+store.dispatch(addProduct("milk"));
